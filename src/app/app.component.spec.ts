30be6245c99607810f8dbe9a35c4826898dcf5f1
jostincha812 <jostin.cha812@gmail.com>
2019-03-11 20:11:25 +0200

    updated references that had old project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-image-upload'`, () => {
+  it(`should have as title 'mean-posts'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-image-upload');
+    expect(app.title).toEqual('mean-posts');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-image-upload!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mean-posts!');
   });
 });
